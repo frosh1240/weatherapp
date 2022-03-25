4d0b1700c406c7c7d4daf2eb2e1527827bedb6af
frosh1240 <camr2669@gmail.com>
2022-03-25 17:44:56 -0500

    second commit

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { WeatherData } from '@app/shared/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WeatherComponent implements OnInit {
+  @Input() public weather!: WeatherData;
+  constructor() {}
 
-  constructor() { }
+  public BASE_URL_IMG = 'http://openweathermap.org/img/wn';
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
